8beea3e2e46c0c5a00306ea8a0771e0304f5d858
Hasemi2 <h171754@gmail.com>
2025-02-16 03:38:20 +0900

    실습

--- src/main/java/com/practice/shop/order/service/OrderService.java
+++ src/main/java/com/practice/shop/order/service/OrderService.java
@@ -5,15 +5,23 @@ import com.practice.shop.delivery.domain.DeliveryStatus;
 import com.practice.shop.item.domain.Item;
 import com.practice.shop.item.service.ItemService;
 import com.practice.shop.member.domain.Member;
+import com.practice.shop.member.domain.QMember;
 import com.practice.shop.member.service.MemberService;
 import com.practice.shop.order.domain.Order;
 import com.practice.shop.order.domain.OrderItem;
 import com.practice.shop.order.domain.OrderRepository;
 import com.practice.shop.order.domain.OrderSearch;
+import com.practice.shop.order.domain.OrderStatus;
+import com.practice.shop.order.domain.QOrder;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
 import java.util.List;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 
 @Service
 @RequiredArgsConstructor
@@ -23,6 +31,7 @@ public class OrderService {
   private final MemberService memberService;
   private final OrderRepository orderRepository;
   private final ItemService itemService;
+  private final EntityManager em;
 
   @Transactional
   public Long order(Long memberId, Long itemId, int count) {
@@ -47,10 +56,34 @@ public class OrderService {
     order.cancel();
   }
 
-  // todo: querydsl 연동
   public List<Order> findOrders(OrderSearch orderSearch) {
-    return null;
+    QOrder order = QOrder.order;
+    QMember member = QMember.member;
+
+    JPAQueryFactory query = new JPAQueryFactory(em);
+
+    return query
+        .select(order)
+        .from(order)
+        .join(order.member, member)
+        .where(statusEq(orderSearch.getOrderStatus()), memberNameLike(orderSearch.getMemberName()))
+        .limit(1000)
+        .fetch();
+
   }
 
+  private BooleanExpression statusEq(OrderStatus orderStatus) {
+    if (Objects.isNull(orderStatus)) {
+      return null;
+    }
+    return QOrder.order.status.eq(orderStatus);
+  }
+
+  private BooleanExpression memberNameLike(String name) {
+    if (StringUtils.hasText(name)) {
+      return null;
+    }
+    return QMember.member.name.like(name);
+  }
 
 }
